11321b439a4146cd1726592e5fe5a583ebb76b96
mikeyyb22 <mcgeheemichael22@gmail.com>
2026-02-10 22:26:26 -0300

    useState & useEffect

--- flashcard-app/src/flashcard.jsx
+++ flashcard-app/src/flashcard.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
+
 function Flashcard({ word, level }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  useEffect(() => {
+    console.log(`Flashcard loaded: ${word}`)
+  }, []);
+  
   return (
-    <div>
-      <h2>{word}</h2>
-      <p>Level {level}</p>
+    <div
+      onClick={() => setIsFlipped(!isFlipped)}
+      style={{
+        border: "1px solid black",
+        padding: "16px",
+        marginBottom: "10px",
+        cursor: "pointer",
+      }}
+    >
+      {isFlipped ? (
+        <p>Level {level}</p>
+      ) : (
+        <h2>{word}</h2>
+      )}
     </div>
   );
 }
