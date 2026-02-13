fec517810773445fd508586532f94914b6fd60f8
mikeyyb22 <mcgeheemichael22@gmail.com>
2026-02-13 10:36:39 -0300

    Added card counter and useState

--- flashcard-app/src/App.jsx
+++ flashcard-app/src/App.jsx
@@ -1,25 +1,38 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import Flashcard from "./flashcard";
 
 function App() {
+  useEffect(() => {
+    console.log("App loaded!");
+  }, []);
+
   const [flippedId, setFlippedId] = useState(null);
-  
-  const flashcards = [
+  // setFlashcards for when we start creating and updating cards dynamically
+  const [flashcards, setFlashcards] = useState([
     { id: 1, word: "apple", level: 1 },
     { id: 2, word: "banana", level: 2 },
     { id: 3, word: "orange", level: 3 }
-  ];
+  ]);
+  const [reviewCount, setReviewCount] = useState(0);
+
+  // When card flips, increase review count
+  const handleFlip = (id) => {
+    setFlippedId(id);
+    setReviewCount(prev => prev + 1);
+  }
 
   return (
     <div>
       <h1>My Flashcards</h1>
+      <h2>Cards reviewed: {reviewCount}</h2>
       {flashcards.map(card => (
         <Flashcard
           key={card.id}
           word={card.word}
           level={card.level}
-          isFlipped={flippedId == card.id}
-          onFlip={() => setFlippedId(card.id)}
+          isFlipped={flippedId === card.id}
+          onFlip={() => handleFlip(card.id)}
         />
       ))}
     </div>
